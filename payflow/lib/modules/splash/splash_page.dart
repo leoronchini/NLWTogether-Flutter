4017773819a7ab34c7526a67feab9e46a3753346
Leonardo Ronchini <leonardo.ronchini@weclever.co>
2021-06-23 09:02:13 -0300

    Importing assets and creating first module

---
+++ payflow/lib/modules/splash/splash_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SpashPage extends StatelessWidget {
+  const SpashPage({Key? key}) : super(key: key);
+//49mins
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
